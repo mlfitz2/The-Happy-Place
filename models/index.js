ed95d619af86e23a3d7c42cdcc58872475869d19
Ben Fok <benfok8@gmail.com>
2022-03-25 10:14:22 -0700

    adding public flag on posts, adding homepage routes

--- models/index.js
+++ models/index.js
@@ -1,11 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// A User can have many posts
 User.hasMany(Post, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
+// Posts belong to Users
 Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
