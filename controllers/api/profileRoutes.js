b53dabcc9354684f26a9baad51cc4c11e88c7620
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-03-26 13:40:22 -0700

    profileRoutes.js file created

---
+++ controllers/api/profileRoutes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const { request } = require('express');
+const { User, Post } = require('../../models');
+// const withAuth = require('../../utils/auth');
+
+router.get('/:user', async (req, res) => {
+    try {
+
+        let data =  {
+            model: Post, 
+            where: { public: true }
+        }
+
+        if ( req.params.user.toLowerCase() === req.session.username.toLowerCase()) {
+            //Below: for testing purposes
+            // if ( req.params.user.toLowerCase() === 'john') {
+            data = {
+                model: Post
+            }
+        }
+
+        const userData = await User.findOne({ where: { name: req.params.user }, 
+            include: [
+                data
+            ] 
+        });
+
+        if (!userData) {
+            res.status(400).json({ message: 'No such user exists.'});
+            return;
+        }
+
+        res.status(200).json(userData);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: `${error}` });
+    }
+});
+
+module.exports = router;
