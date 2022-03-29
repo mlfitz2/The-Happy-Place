5ebe17b79c32e01602d8c322853625e85d1f3c38
Jon Burch <jonhb82@gmail.com>
2022-03-29 10:42:07 -0700

    Merged over

---
+++ public/js/login.js
@@ -0,0 +1,26 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#userName').value.trim();
+
+    const password = document.querySelector('#password').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/user/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        console.log('Response Okay');
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+
+  document
+  .querySelector('#loginForm')
+  .addEventListener('submit', loginFormHandler);
