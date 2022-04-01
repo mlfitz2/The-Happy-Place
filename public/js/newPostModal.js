c37e4dd7bb2b132cfcf769e1f32f5039d8ee1904
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-03-31 20:54:44 -0700

    styling on modal

--- public/js/newPostModal.js
+++ public/js/newPostModal.js
@@ -7,7 +7,6 @@ const modalControl = function(){
     // When the user clicks the button, open the modal 
     btn.addEventListener('click', function() {
         modal.style.display = "block";
-        
     });
 
     // When the user clicks on <span> (x), close the modal
